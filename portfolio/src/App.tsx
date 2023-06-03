f0d2df245620f0700c72a1e848edb0e98ec16de2
Raphael Vieira Rossi <raphael.rossi@g.globo>
2023-06-03 19:31:21 -0300

    test 1

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import rapha from './raphael-rossi.svg';
 import './App.css';
+import './style.scss';
+import Button from '@mui/material/Button';
+
 
 function clickMe() {
   alert("You clicked me again 2!");
 }
 
 function App() {
+  console.log(process.env)
   return (
     <div className="App">
       <header className="App-header">
         <img src={rapha} className="App-logo" alt="logo" />
       </header>
       <div>
-        <button onClick={clickMe}>Button</button>
+        <Button variant="contained" onClick={clickMe}>Button</Button>
+      </div>
+      <div>
+        <h1>Olá Mundo!</h1>
+        <h1>{process.env.REACT_APP_APIKey}</h1>
       </div>
     </div>
   );
