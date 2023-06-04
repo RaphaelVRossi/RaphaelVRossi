102ae731f5f0ba7968debdc3e40adf64406c3812
Raphael Vieira Rossi <raphael.rossi@g.globo>
2023-06-04 20:32:02 -0300

    fix for big change

--- portfolio/src/components/app/App.tsx
+++ portfolio/src/components/app/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {ReactComponent as ReactLogo} from './raphael-rossi.svg';
 import './App.css';
 import '../../styles/style.scss';
-import Button from '@mui/material/Button';
-import Logo from '../logo/logo'
 import Contact from '../contact/Contact'
 import Footer from '../footer/Footer'
 import Home from '../home/Home'
 import Navigation from '../navigation//Navigation'
 import Projects from '../projects/Projects'
 
-
-function clickMe() {
-  alert("You clicked me again 2!");
-}
 
 function App() {
   console.log(process.env)
