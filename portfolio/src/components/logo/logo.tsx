2aef14d3eb3588201fb6f385bd1b0955e45a2ce4
Raphael Vieira Rossi <raphael.rossi@g.globo>
2023-06-04 20:29:09 -0300

    Big change

---
+++ portfolio/src/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ReactComponent as LogoSvg} from '../../assets/images/raphael-rossi.svg';
+import './logo.scss'
+
+function Logo() {
+  return (
+    <LogoSvg className="logo"/> 
+  ) 
+}
+
+export default Logo;
