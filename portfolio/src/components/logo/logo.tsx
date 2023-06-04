533862e24a2512d1a2646c1846ace15436e003ac
Raphael Vieira Rossi <raphael.rossi@g.globo>
2023-06-04 20:34:19 -0300

    fix footer

--- portfolio/src/components/logo/logo.tsx
+++ portfolio/src/components/logo/logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ReactComponent as LogoSvg} from '../../assets/images/raphael-rossi.svg';
-import './logo.scss'
+import './Logo.scss'
 
 function Logo() {
   return (
