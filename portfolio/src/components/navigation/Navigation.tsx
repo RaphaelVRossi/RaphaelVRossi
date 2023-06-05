534740dc75e65f8612104bf618e3310bc6c19e09
Raphael Vieira Rossi <raphael.rossi@g.globo>
2023-06-04 23:00:55 -0300

    a lot of css

--- portfolio/src/components/navigation/Navigation.tsx
+++ portfolio/src/components/navigation/Navigation.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import './Navigation.scss'
+import {ReactComponent as PhoneIcon} from '../../assets/icons/phone-icon.svg'
+import {ReactComponent as EmailIcon} from '../../assets/icons/email-icon.svg'
+import {ReactComponent as GitHubIcon} from '../../assets/icons/github-icon.svg'
+import {ReactComponent as LinkedinIcon} from '../../assets/icons/linkedin-icon.svg'
 
 function Navigation() {
   return (
     <section className="navigation">
-      <div className="square">
-        <p>
-          about me
-        </p>
-      </div>
-      <div className="square">
-        <p>
-          projects
-        </p>
-      </div>
-      <div className="square">
-        <p>
-          contact
-        </p>
-      </div>
+        <div className="square">
+          <p>
+            About Me
+          </p>
+        </div>
+        <div className="square">
+          <p>
+            Projects 
+          </p>
+        </div>
+        <div className="square">
+          <p>
+            Contact 
+          </p>
+        </div>
+        <div className="square-2">
+        <PhoneIcon className="icon"/>
+        </div>
+        <div className="square-2">
+        <EmailIcon className="icon"/>
+        </div>
+        <div className="square-2">
+        <GitHubIcon className="icon"/>
+        </div>
+        <div className="square-2">
+        <LinkedinIcon className="icon"/>
+        </div>
     </section>
   )
 }
