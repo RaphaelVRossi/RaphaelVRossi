fce898faab1af26f245b98578812128556be4067
Raphael Vieira Rossi <raphael.rossi@g.globo>
2024-03-25 08:20:17 -0300

    fixed navigation css

--- portfolio/src/components/home/Home.tsx
+++ portfolio/src/components/home/Home.tsx
@@ -7,13 +7,28 @@ function Home() {
       <section className="aboutme">
         <div />
         <h1 className="larger-font">Who Am I?</h1>
-        <p className="medium-font"> Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
+        <p className="medium-font">
+        I am a tech enthusiast with a passion for code, debugging, and retro gaming. Ever since I was a kid, I have been fascinated by how computers work and how they can be used to create amazing things.
+        </p>
+        <p className="medium-font">
+          I started programming as a teenager and quickly fell in love with the art of turning ideas into code. I love the challenge of solving complex problems and finding elegant solutions. I am also a big fan of retro games, especially Final Fantasy and Pokemon.
+        </p>
+        <p className="medium-font">
+          In addition to programming and gaming, I am also passionate about working in a simple yet efficient environment, which is why I love using the terminal, especially VIM!
+        </p>
+        <p className="medium-font">
+          I am currently working as a full-stack developer at a large entertainment company, using Python and Go for the backend and React with Material UI for the frontend.
+        </p>
+        <p className="medium-font">
+          I am a contributor to open source projects and I am always looking for new projects to contribute to.
+          Outside of work, I enjoy spending time with my wife and friends, playing video games, and collecting and playing Magic: the Gathering cards.
+          I am excited to see what the future holds for technology.</p>
       </section>
 
       <div className="home-content">
         <Portfolio />
       </div>
-    </section>
+    </section >
   )
 }
 
