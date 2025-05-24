4f3509188681e5048ed2ab0ce435ede1eef6167e
Raphael Rossi <me@raphaelrossi.io>
2025-05-24 11:21:58 -0300

    feat: carousel on talks area

--- portfolio/src/components/talks/Talks.tsx
+++ portfolio/src/components/talks/Talks.tsx
@@ -1,48 +1,77 @@
 import './Talks.scss';
+import Carousel from '../carousel/Carousel';
 
-function Projects() {
+const hacktoberFest = (
+  <div className="talk">
+    <h1 className="medium-font">HacktoberFest na Globo: Como funciona?</h1>
+    <iframe src="https://www.youtube.com/embed/akUO6jP-HJ8" title="GlobotechCast | Episódio 54 – HacktoberFest na Globo: Como funciona?" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <p className="small-font">
+      #openSource #globo #hacktoberFest
+    </p>
+  </div>
+)
+
+const gitlabCi = (
+  <div className="talk">
+    <h1 className="medium-font">Gitlab-CI: O que é e por que utilizar?</h1>
+    <iframe src="https://www.youtube.com/embed/DcjGNgso314" title="Engcast #07 - Gitlab-CI: O que é e por que utilizar?" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <p className="small-font">
+      #gitlab-ci #engdb #devops
+    </p>
+  </div>
+)
+
+const microservicos = (
+  <div className="talk">
+    <h1 className="medium-font">Microsserviços modulares em produção</h1>
+    <iframe src="https://www.youtube.com/embed/nItgmEkZqEY" title="Engcast #02 - Microsserviços modulares em produção" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <p className="small-font">
+      #microservicos #engdb #develop
+    </p>
+  </div>
+)
+
+const engeek2 = (
+  <div className="talk">
+    <h1 className="medium-font">Engeek Tech Beer - Episodio 02</h1>
+    <iframe src="https://www.youtube.com/embed/SNYWsZDTYs0" title="Engeek Tech Beer - Episodio 02" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <p className="small-font">
+      #engdb
+    </p>
+  </div>
+)
+
+const engeek1 = (
+  <div className="talk">
+    <h1 className="medium-font">Engeek Tech Beer -  Episodio 01</h1>
+    <iframe src="https://www.youtube.com/embed/WTUIqA5Jrbg" title="Engeek Tech Beer -  Episodio 01" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <p className="small-font">
+      #engdb
+    </p>
+  </div>
+)
+
+function Talks() {
   return (
-    <section className="projects" id="projects">
+    <section className="talks" id="talks">
       <h1 className="page-title">Talks</h1>
-      <div className="project-list">
-        <div className="project">
-          <h1 className="medium-font">HacktoberFest na Globo: Como funciona?</h1>
-<iframe width="640" height="360" src="https://www.youtube.com/embed/akUO6jP-HJ8" title="GlobotechCast | Episódio 54 – HacktoberFest na Globo: Como funciona?" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <p className="small-font">
-            #openSource #globo #hacktoberFest
-          </p>
-        </div> 
-        <div className="project">
-          <h1 className="medium-font">Gitlab-CI: O que é e por que utilizar?</h1>
-<iframe width="640" height="360" src="https://www.youtube.com/embed/DcjGNgso314" title="Engcast #07 - Gitlab-CI: O que é e por que utilizar?" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <p className="small-font">
-            #gitlab-ci #engdb #devops
-          </p>
-        </div> 
-        <div className="project">
-          <h1 className="medium-font">Microsserviços modulares em produção</h1>
-          <iframe width="640" height="360" src="https://www.youtube.com/embed/nItgmEkZqEY" title="Engcast #02 - Microsserviços modulares em produção" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <p className="small-font">
-            #microservicos #engdb #develop
-          </p>
-        </div> 
-        <div className="project">
-          <h1 className="medium-font">Engeek Tech Beer - Episodio 02</h1>
-          <iframe width="640" height="360" src="https://www.youtube.com/embed/SNYWsZDTYs0" title="Engeek Tech Beer - Episodio 02" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <p className="small-font">
-            #engdb
-          </p>
-        </div> 
-        <div className="project">
-          <h1 className="medium-font">Engeek Tech Beer -  Episodio 01</h1>
-          <iframe width="640" height="360" src="https://www.youtube.com/embed/WTUIqA5Jrbg" title="Engeek Tech Beer -  Episodio 01" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <p className="small-font">
-            #engdb
-          </p>
-        </div> 
+      <div className="talks-list">
+        {
+          Carousel(
+            {
+              components: [
+                hacktoberFest,
+                gitlabCi,
+                microservicos,
+                engeek2,
+                engeek1
+              ]
+            }
+          )
+        }
       </div>
     </section>
   )
 }
 
-export default Projects;
+export default Talks;
