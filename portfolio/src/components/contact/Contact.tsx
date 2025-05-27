7a630aba378df8c1d1aada05925ec8f3ff860ab7
Raphael Rossi <me@raphaelrossi.io>
2025-05-27 08:12:59 -0300

    fix: fixed some text

--- portfolio/src/components/contact/Contact.tsx
+++ portfolio/src/components/contact/Contact.tsx
@@ -6,7 +6,10 @@ function Contact() {
       <h1 className="page-title">Contact</h1>
       <section className="contactme">
         <p className="medium-font">
-          Thanks for stopping by! I would love to chat about programming languages, architecture and structure of code, new AI topics and other interesting IT subjects. Feel free to send me a email or find me on my social media.
+          Thanks for stopping by! I would love to chat about programming languages, code architecture and structure, new AI topics and any other interesting IT subjects.
+        </p>
+        <p className="medium-font">
+          Feel free to email me or hit me up on my social media channels.
         </p>
       </section>
     </section>
